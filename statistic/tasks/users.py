7511e61a614b1de0e39bed393d79a203e995aff4
agap <agap18@list.ru>
2025-03-09 11:20:36 +0300

    [Task-3] Refactor tasks to support multiple date ranges and metrics
    Updated task functions to process multiple date ranges concurrently using asyncio. Introduced a structured format for metrics response and streamlined their output. Commented legacy code for potential future reference.

--- statistic/tasks/users.py
+++ statistic/tasks/users.py
@@ -1,5 +1,6 @@
+import asyncio
 from datetime import date
-from typing import Dict
+from typing import Dict, List, Tuple
 
 from core.database import get_session
 from repositories.users import (
@@ -7,46 +8,120 @@ from repositories.users import (
     get_users_count_with_filters,
 )
 from statistic.celery_app import app
-import asyncio
+from statistic.helpers import format_metrics_response
+
+# @app.task
+# def calculate_registered_users(dt_gt: date, dt_lt: date)-> List[Dict[str, str]]:
+#     async def _calculate_inner():
+#         async with get_session() as session:
+#             users_count: int = await get_registered_users_count(
+#                 session=session, dt_gt=dt_gt, dt_lt=dt_lt
+#             )
+#             return {"registered_users_count": users_count}
+#
+#     return asyncio.run(_calculate_inner())
 
 
 @app.task
-def calculate_registered_users(dt_gt: date, dt_lt: date) -> Dict[str, int]:
+def calculate_registered_users(
+    date_ranges: List[Tuple[date, date]]
+) -> List[Dict[str, str]]:
     async def _calculate_inner():
+        coroutines: List = []
         async with get_session() as session:
-            users_count: int = await get_registered_users_count(
-                session=session, dt_gt=dt_gt, dt_lt=dt_lt
+            for date_range in date_ranges:
+                dt_gt, dt_lt = date_range
+                coroutines.append(
+                    get_registered_users_count(
+                        session=session, dt_gt=dt_gt, dt_lt=dt_lt
+                    )
+                )
+
+            users_count_values: List[int] = await asyncio.gather(*coroutines)
+            return format_metrics_response(
+                date_range=date_range,
+                metric_name="registered_users_count",
+                metric_values=users_count_values,
             )
-            return {"registered_users_count": users_count}
 
     return asyncio.run(_calculate_inner())
 
 
+# @app.task
+# def calculate_registered_and_deposit_users(
+#     dt_gt: date, dt_lt: date
+# ) -> Dict[str, int]:
+#     async def _calculate_inner():
+#         async with get_session() as session:
+#             users_count: int = await get_users_count_with_filters(
+#                 session=session, dt_gt=dt_gt, dt_lt=dt_lt
+#             )
+#             return {"registered_and_deposit_users_count": users_count}
+#
+#     return asyncio.run(_calculate_inner())
 @app.task
 def calculate_registered_and_deposit_users(
-        dt_gt: date, dt_lt: date
-) -> Dict[str, int]:
+    date_ranges: List[Tuple[date, date]]
+) -> List[Dict[str, str]]:
     async def _calculate_inner():
+        coroutines: List = []
         async with get_session() as session:
-            users_count: int = await get_users_count_with_filters(
-                session=session, dt_gt=dt_gt, dt_lt=dt_lt
+            for date_range in date_ranges:
+                dt_gt, dt_lt = date_range
+                coroutines.append(
+                    get_users_count_with_filters(
+                        session=session, dt_gt=dt_gt, dt_lt=dt_lt
+                    )
+                )
+            users_count_values: List[int] = await asyncio.gather(*coroutines)
+            return format_metrics_response(
+                date_range=date_range,
+                metric_name="registered_and_deposit_users_count",
+                metric_values=users_count_values,
             )
-            return {"registered_and_deposit_users_count": users_count}
 
     return asyncio.run(_calculate_inner())
 
 
+# @app.task
+# def calculate_registered_and_not_rollbacked_deposit_users(
+#     dt_gt: date, dt_lt: date
+# ) -> Dict[str, int]:
+#     async def _calculate_inner():
+#         async with get_session() as session:
+#             users_count: int = await get_users_count_with_filters(
+#                 session=session,
+#                 dt_gt=dt_gt,
+#                 dt_lt=dt_lt,
+#                 exclude_rollbacked=True,
+#             )
+#             return {
+#                 "registered_and_not_rollbacked_deposit_users_count": users_count
+#             }
+#
+#     return asyncio.run(_calculate_inner())
 @app.task
 def calculate_registered_and_not_rollbacked_deposit_users(
-        dt_gt: date, dt_lt: date
-) -> Dict[str, int]:
+    date_ranges: List[Tuple[date, date]]
+) -> List[Dict[str, str]]:
     async def _calculate_inner():
+        coroutines: List = []
         async with get_session() as session:
-            users_count: int = await get_users_count_with_filters(
-                session=session, dt_gt=dt_gt, dt_lt=dt_lt, exclude_rollbacked=True
+            for date_range in date_ranges:
+                dt_gt, dt_lt = date_range
+                coroutines.append(
+                    get_users_count_with_filters(
+                        session=session,
+                        dt_gt=dt_gt,
+                        dt_lt=dt_lt,
+                        exclude_rollbacked=True,
+                    )
+                )
+            users_count_values: List[int] = await asyncio.gather(*coroutines)
+            return format_metrics_response(
+                date_range=date_range,
+                metric_name="registered_and_not_rollbacked_deposit_users_count",
+                metric_values=users_count_values,
             )
-            return {
-                "registered_and_not_rollbacked_deposit_users_count": users_count
-            }
 
     return asyncio.run(_calculate_inner())
